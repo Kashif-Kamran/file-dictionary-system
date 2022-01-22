247e2472fd3a4c38a460d77d61450ffe1cc3deff
Kashif-Kamran <80104444+Kashif-Kamran@users.noreply.github.com>
2022-01-23 01:13:36 +0500

    Vocabulary Class implemented with following functionalities :
    	1. 	It has ability to populate the Tree
    
    	2.	It has ability to search for word (String)

--- FileSystemProject/src/main/java/FileHandle.java
+++ FileSystemProject/src/main/java/FileHandle.java
@@ -1,36 +1,39 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class FileHandle
 {
-	String fileName;
-	List<String> wordList;
+	String fileName; // File being read
+	List<String> wordList; // List of words in file
 
 	// Constructor
-	public FileHandle(String fileName)
+	public FileHandle(String fileName) throws Exception
 	{
 		super();
 		this.fileName = fileName;
-		wordList=new LinkedList<String>();
+		wordList = new LinkedList<String>();
+		this.loadWordList();
 	}
 
+	// Function to load the words of file into wordList
 	public void loadWordList() throws Exception
 	{
 		try
 		{
-			File file= new File(this.fileName);
+			File file = new File(this.fileName);
 			Scanner reader = new Scanner(file);
 			String tempWord;
 			while (reader.hasNext())
 			{
-				tempWord=reader.next();
-				tempWord=this.removePollution(tempWord);
-				tempWord=tempWord.toLowerCase();
+				tempWord = reader.next();
+				tempWord = this.removePollution(tempWord);
+				tempWord = tempWord.toLowerCase();
 				wordList.add(tempWord);
-				
+
 			}
 			reader.close();
 
@@ -39,26 +42,51 @@ public class FileHandle
 			throw new FileNotFoundException(this.fileName + " File Not Found");
 		}
 	}
-	
+
+	// Function to remove the pollution
 	private String removePollution(String pollutedWord)
 	{
-		pollutedWord=pollutedWord.replace(",","");
-		pollutedWord=pollutedWord.replace("?","");
-		pollutedWord=pollutedWord.replace(")","");
-		pollutedWord=pollutedWord.replace("(","");
-		pollutedWord=pollutedWord.replace(":","");
-		pollutedWord=pollutedWord.replace(".","");
-		String pollutionFreeWord=pollutedWord;
+		pollutedWord = pollutedWord.replace(",", "");
+		pollutedWord = pollutedWord.replace("?", "");
+		pollutedWord = pollutedWord.replace(")", "");
+		pollutedWord = pollutedWord.replace("(", "");
+		pollutedWord = pollutedWord.replace(":", "");
+		pollutedWord = pollutedWord.replace(".", "");
+		String pollutionFreeWord = pollutedWord;
 		return pollutionFreeWord;
 	}
-	
+
+	public String getFileName()
+	{
+		return fileName;
+	}
+
+	public List<String> getWordList()
+	{
+		return wordList;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "FileHandle [fileName=" + fileName + ", wordList=" + wordList + "]";
+	}
+
 	public static void main(String[] args)
 	{
-		FileHandle file = new FileHandle("./textFiles/test.txt");
+
 		try
 		{
-			file.loadWordList();
-			
+			FileHandle file = new FileHandle("./textFiles/test.txt");
+			LinkedList<String> list = (LinkedList<String>) file.getWordList();
+			Iterator<String>itr = list.iterator();
+			int i = 1;
+			while (itr.hasNext())
+			{
+				System.out.println(i + " : " + itr.next());
+				i++;
+			}
+
 		} catch (Exception exc)
 		{
 			System.out.println(exc.getMessage());
---
+++ FileSystemProject/src/main/java/Vocabulary.java
@@ -0,0 +1,64 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class Vocabulary
+{
+	TreeSet<String> vocab;
+	FileHandle file;
+
+	public Vocabulary(String fileName) throws Exception
+	{
+		super();
+		vocab = new TreeSet<String>();
+		this.file = new FileHandle(fileName);
+	}
+
+	public void populateVocabulary()
+	{
+		LinkedList<String> wordsList = (LinkedList<String>) file.getWordList();
+		Iterator<String> itr = wordsList.iterator();
+		while (itr.hasNext())
+		{
+			vocab.add(itr.next());
+
+		}
+	}
+
+	public TreeSet<String> getVocabTree()
+	{
+		return vocab;
+	}
+
+	public boolean searchWord(String str)
+	{
+		Iterator<String> itr = vocab.iterator();
+		while (itr.hasNext())
+		{
+			if (str.compareToIgnoreCase(itr.next()) == 0)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+// For testing
+	public static void main(String[] args)
+	{
+		try
+		{
+			Vocabulary vocab = new Vocabulary("./textFiles/test.txt");
+			vocab.populateVocabulary();
+			Iterator<String> itr = vocab.getVocabTree().iterator();
+
+			while (itr.hasNext())
+			{
+				System.out.println(itr.next());
+			}
+		} catch (Exception exc)
+		{
+			System.out.println(exc.getMessage());
+		}
+
+	}
+}
